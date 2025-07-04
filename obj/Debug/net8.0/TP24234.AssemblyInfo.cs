d1f3154f061ab2a6bbc33994277cf609f2cac870
BRUNO EZEQUIEL ROTH <49004085@ort>
2025-07-04 15:05:24 -0300

    Inicializado completo

--- obj/Debug/net8.0/TP24234.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP24234.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP24234")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53dbfda69ac840778e9cb9527bde0082bb5b44e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP24234")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP24234")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
